dcf5fec3b93a601425c9d7b9a3d4975c81c0f541
daniilkuz <d.d.kuzovlev@gmail.com>
2024-06-11 23:56:06 +0700

    moved connection initialization from Set to New for Client

--- client/client.go
+++ client/client.go
@@ -3,6 +3,8 @@ package client
 import (
 	"bytes"
 	"context"
+	"io"
+	"log"
 	"net"
 
 	"github.com/tidwall/resp"
@@ -10,28 +12,30 @@ import (
 
 type Client struct {
 	addr string
+	conn net.Conn
 }
 
 func New(addr string) *Client {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return &Client{
 		addr: addr,
+		conn: conn,
 	}
 }
 
 func (c *Client) Set(ctx context.Context, key string, val string) error {
-	conn, err := net.Dial("tcp", c.addr)
-	if err != nil {
-		return err
-	}
-
-	var buf bytes.Buffer
-	wr := resp.NewWriter(&buf)
+	buf := &bytes.Buffer{}
+	wr := resp.NewWriter(buf)
 	wr.WriteArray([]resp.Value{
 		resp.StringValue("SET"),
 		resp.StringValue(key),
 		resp.StringValue(val),
 	})
 	// fmt.Printf("%s", buf.String())
-	_, err = conn.Write(buf.Bytes())
+	// _, err = conn.Write(buf.Bytes())
+	_, err := io.Copy(c.conn, buf)
 	return err
 }
