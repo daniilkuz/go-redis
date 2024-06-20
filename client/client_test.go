424b66ca304bc7c2628cba018e2e9544e3043bbd
daniilkuz <d.d.kuzovlev@gmail.com>
2024-06-20 23:58:17 +0700

    started adding functionality to client_test

--- client/client_test.go
+++ client/client_test.go
@@ -1,1 +1,31 @@
 package client
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+)
+
+func TestNewClient(t *testing.T) {
+
+	client, err := client.New("localhost:5001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < 10; i++ {
+
+		fmt.Println("SET => ", fmt.Sprintf("bar %d", i))
+		if err := client.Set(context.TODO(), fmt.Sprint(i), fmt.Sprintf("bar %d", i)); err != nil {
+			log.Fatal(err)
+		}
+		val, err := client.Get(context.TODO(), fmt.Sprint(i))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("GET => ", val)
+	}
+	time.Sleep(time.Second)
+	fmt.Println(server.kv.data)
+}
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"go-redis/client"
 	"log"
 	"log/slog"
 	"net"
@@ -125,22 +123,4 @@ func main() {
 	}()
 	time.Sleep(time.Second)
 
-	client, err := client.New("localhost:5001")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for i := 0; i < 10; i++ {
-
-		fmt.Println("SET => ", fmt.Sprintf("bar %d", i))
-		if err := client.Set(context.TODO(), fmt.Sprint(i), fmt.Sprintf("bar %d", i)); err != nil {
-			log.Fatal(err)
-		}
-		val, err := client.Get(context.TODO(), fmt.Sprint(i))
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("GET => ", val)
-	}
-	time.Sleep(time.Second)
-	fmt.Println(server.kv.data)
 }
