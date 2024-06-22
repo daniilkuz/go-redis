08263c3c0505462d89be364c8cec4efbc6a7dd08
daniilkuz <d.d.kuzovlev@gmail.com>
2024-06-22 23:49:19 +0700

    started adding the second test

--- client/client_test.go
+++ client/client_test.go
@@ -8,6 +8,27 @@ import (
 	"time"
 )
 
+func TestNewClient2(t *testing.T) {
+
+	client, err := New("localhost:5001")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i := 0; i < 10; i++ {
+
+		fmt.Println("SET => ", fmt.Sprintf("bar %d", i))
+		if err := client.Set(context.TODO(), fmt.Sprint(i), fmt.Sprintf("bar %d", i)); err != nil {
+			log.Fatal(err)
+		}
+		val, err := client.Get(context.TODO(), fmt.Sprint(i))
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("GET => ", val)
+	}
+	time.Sleep(time.Second)
+}
+
 func TestNewClient(t *testing.T) {
 
 	client, err := New("localhost:5001")
