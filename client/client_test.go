88f104c0e0dec64a2d2cf6f26e40ebae0627ab0f
daniilkuz <d.d.kuzovlev@gmail.com>
2024-06-23 23:57:09 +0700

    ^started adding the second test

--- client/client_test.go
+++ client/client_test.go
@@ -14,19 +14,16 @@ func TestNewClient2(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for i := 0; i < 10; i++ {
 
-		fmt.Println("SET => ", fmt.Sprintf("bar %d", i))
-		if err := client.Set(context.TODO(), fmt.Sprint(i), fmt.Sprintf("bar %d", i)); err != nil {
-			log.Fatal(err)
-		}
-		val, err := client.Get(context.TODO(), fmt.Sprint(i))
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("GET => ", val)
+	if err := client.Set(context.TODO(), "foo", 1); err != nil {
+		log.Fatal(err)
 	}
-	time.Sleep(time.Second)
+	// val, err := client.Get(context.TODO(), fmt.Sprint(i))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("GET => ", val)
+
 }
 
 func TestNewClient(t *testing.T) {
