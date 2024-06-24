ea3d4de115799e5d15be99721989b9fc113c1458
daniilkuz <d.d.kuzovlev@gmail.com>
2024-06-24 23:58:21 +0700

    fix

--- client/client_test.go
+++ client/client_test.go
@@ -6,9 +6,18 @@ import (
 	"log"
 	"testing"
 	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 func TestNewClient2(t *testing.T) {
+
+	ctx := context.Background()
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 
 	client, err := New("localhost:5001")
 	if err != nil {
