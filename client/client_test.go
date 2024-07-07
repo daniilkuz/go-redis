ba1c35cc88a6bffcaef01b9d47bbfdb30c182b33
daniilkuz <d.d.kuzovlev@gmail.com>
2024-07-07 22:50:02 +0300

    added TestNewClient - for one client test

--- client/client_test.go
+++ client/client_test.go
@@ -14,6 +14,24 @@ func TestNewClient(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer client.Close()
+	fmt.Println("SET => ", "bar")
+	if err := client.Set(context.TODO(), "foo", "bar"); err != nil {
+		log.Fatal(err)
+	}
+	val, err := client.Get(context.TODO(), "foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("GET => ", val)
+}
+
+func TestNewClients(t *testing.T) {
+
+	client, err := New("localhost:5001")
+	if err != nil {
+		log.Fatal(err)
+	}
 	for i := 0; i < 10; i++ {
 
 		fmt.Println("SET => ", fmt.Sprintf("bar %d", i))
