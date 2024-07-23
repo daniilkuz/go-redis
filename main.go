b10f0629e8c08ae8c1ecaadf42d490c686b6134f
daniilkuz <d.d.kuzovlev@gmail.com>
2024-07-23 12:44:35 +0300

    small fix

--- main.go
+++ main.go
@@ -72,7 +72,8 @@ func (s *Server) handleMessage(msg Message) error {
 		}
 		_, err := msg.peer.Send(val)
 		if err != nil {
-			slog.Error("peer send error", "err", err)
+			// slog.Error("peer send error", "err", err)
+			return fmt.Errorf("peer send error: %s", err)
 		}
 		// return s.set(v.key, v.val)
 		// slog.Info("someone wants to set a key into the hash table", "key", v.key, "val", v.val)
@@ -83,9 +84,10 @@ func (s *Server) handleMessage(msg Message) error {
 		}
 		_, err := msg.peer.Send(respWriteMap(spec))
 		if err != nil {
-			slog.Error("peer send error", "err", err)
+			// slog.Error("peer send error", "err", err)
+			return fmt.Errorf("peer send error: %s", err)
 		}
-		fmt.Println("This is the hello command from the client")
+		// fmt.Println("This is the hello command from the client")
 	}
 	return nil
 }
