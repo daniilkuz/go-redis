0b0f20030ebb089426d10bc37a74fa090c1d84bb
daniilkuz <d.d.kuzovlev@gmail.com>
2024-07-30 10:39:34 +0300

    changed spec map

--- main.go
+++ main.go
@@ -88,8 +88,11 @@ func (s *Server) handleMessage(msg Message) error {
 	case HelloCommand:
 		fmt.Println("hello handled on server")
 		spec := map[string]string{
-			"server": "redis",
-			"role":   "master",
+			"server":  "redis",
+			"version": "6.0.0",
+			"proto":   "3",
+			"mode":    "standalone",
+			"role":    "master",
 		}
 		_, err := msg.peer.Send(respWriteMap(spec))
 		if err != nil {
