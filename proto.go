b77c209df629c2e330176f03b29b8f075a2b8c8f
daniilkuz <d.d.kuzovlev@gmail.com>
2024-07-30 10:40:20 +0300

    small fix

--- proto.go
+++ proto.go
@@ -73,8 +73,8 @@ func respWriteMap(m map[string]string) []byte {
 	buf := bytes.Buffer{}
 	buf.WriteString("%" + fmt.Sprintf("%d\r\n", len(m)))
 	for k, v := range m {
-		buf.WriteString(fmt.Sprintf("+%s\r\n", k))
-		buf.WriteString(fmt.Sprintf(":%s\r\n", v))
+		buf.WriteString(fmt.Sprintf("%s\r\n", k))
+		buf.WriteString(fmt.Sprintf("%s\r\n", v))
 	}
 	return buf.Bytes()
 }
