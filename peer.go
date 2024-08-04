cd72f8166fb967ff526efa8fd7b7a17b62a69ec5
daniilkuz <d.d.kuzovlev@gmail.com>
2024-08-04 13:29:09 +0300

    started changing peer readLoop function

--- peer.go
+++ peer.go
@@ -38,44 +38,62 @@ func (p *Peer) readLoop() error {
 		if err != nil {
 			log.Fatal(err)
 		}
+
+		var cmd Command
 		if v.Type() == resp.Array {
-			for _, value := range v.Array() {
-				fmt.Printf("this command => %v\n", value)
-				var cmd Command
-				switch value.String() {
-				case CommandSET:
-					if len(v.Array()) != 3 {
-						return fmt.Errorf("invalid number of variables for SET command")
-					}
-					cmd = SetCommand{
-						key: v.Array()[1].Bytes(),
-						val: v.Array()[2].Bytes(),
-					}
+			rawCmd := v.Array()[0]
+			switch rawCmd.String() {
+			case CommandGET:
 
-					// fmt.Printf("got SET cmd %+v\n", cmd)
-				case CommandGET:
-					if len(v.Array()) != 2 {
-						return fmt.Errorf("invalid number of variables for GET command")
-					}
-					cmd = GetCommand{
-						key: v.Array()[1].Bytes(),
-					}
+			case CommandSET:
 
-					// fmt.Printf("got GET cmd %+v\n", cmd)
-				case CommandHELLO:
-					cmd = HelloCommand{
-						value: v.Array()[1].String(),
-					}
-				default:
-					fmt.Printf("got unknown command=>%+v\n", v.Array())
-					// fmt.Println(v.Array())
-					// panic("this command is not being handled")
-				}
-				p.msgCh <- Message{
-					cmd:  cmd,
-					peer: p,
+			case CommandHELLO:
+				cmd = HelloCommand{
+					value: v.Array()[1].String(),
 				}
 			}
+			p.msgCh <- Message{
+				cmd:  cmd,
+				peer: p,
+			}
+			fmt.Println("this is cmd", v.Array()[0])
+			// for _, value := range v.Array() {
+			// 	fmt.Printf("this command => %v\n", value)
+			// 	var cmd Command
+			// 	switch value.String() {
+			// 	case CommandSET:
+			// 		if len(v.Array()) != 3 {
+			// 			return fmt.Errorf("invalid number of variables for SET command")
+			// 		}
+			// 		cmd = SetCommand{
+			// 			key: v.Array()[1].Bytes(),
+			// 			val: v.Array()[2].Bytes(),
+			// 		}
+
+			// 		// fmt.Printf("got SET cmd %+v\n", cmd)
+			// 	case CommandGET:
+			// 		if len(v.Array()) != 2 {
+			// 			return fmt.Errorf("invalid number of variables for GET command")
+			// 		}
+			// 		cmd = GetCommand{
+			// 			key: v.Array()[1].Bytes(),
+			// 		}
+
+			// 		// fmt.Printf("got GET cmd %+v\n", cmd)
+			// 	case CommandHELLO:
+			// 		cmd = HelloCommand{
+			// 			value: v.Array()[1].String(),
+			// 		}
+			// 	default:
+			// 		fmt.Printf("got unknown command=>%+v\n", v.Array())
+			// 		// fmt.Println(v.Array())
+			// 		// panic("this command is not being handled")
+			// 	}
+			// 	p.msgCh <- Message{
+			// 		cmd:  cmd,
+			// 		peer: p,
+			// 	}
+			// }
 		}
 	}
 	return nil
