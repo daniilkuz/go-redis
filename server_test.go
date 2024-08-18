117aa17650fafe0523ad19757aed8d9873c441bf
daniilkuz <d.d.kuzovlev@gmail.com>
2024-08-18 15:49:54 +0300

    added TestOfficialRedisClient

--- server_test.go
+++ server_test.go
@@ -10,8 +10,63 @@ import (
 	"testing"
 	"time"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/tidwall/resp"
 )
+
+func TestOfficialRedisClient(t *testing.T) {
+	listenAddr := ":5001"
+	server := NewServer(Config{
+		ListenAddr: listenAddr,
+	})
+	go func() {
+		log.Fatal(server.Start())
+	}()
+	time.Sleep(time.Second)
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("localhost%s", listenAddr),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	// fmt.Println(rdb)
+	// fmt.Println("this is working :)")
+
+	// key := "foo"
+	// val := "bar"
+
+	testCases := map[string]string{
+		"foo":  "bar",
+		"I":    "would",
+		"know": "if",
+		"you":  "told me",
+	}
+
+	for key, val := range testCases {
+		err := rdb.Set(context.Background(), key, val, 0).Err()
+		if err != nil {
+			// panic(err)
+			t.Fatal(err)
+		}
+
+		newVal, err := rdb.Get(context.Background(), key).Result()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if val != newVal {
+			t.Fatalf("expected %v, but got %v instead", val, newVal)
+		}
+	}
+
+	// fmt.Printf("got this value %v\n", val)
+
+	// val, err := rdb.Get(context.TODO(), "key").Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(val)
+}
 
 func TestFooBar(t *testing.T) {
 	buf := &bytes.Buffer{}
